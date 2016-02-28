60999fb1d52e1acd30ed98ec0497e7b957742778
emilanca <emil.anca@ymail.com>
2016-02-28 23:32:39 +0200

    - added sample code for multiple adds

--- js/main.js
+++ js/main.js
@@ -35,6 +35,8 @@
 		// create a message to display in our view
 		$scope.message = 'Despre noi';
 		$scope.anunturi_content = 'shalala';
+		//populate_anunturi()
+
 	});
 
 	ocolApp.controller('licitatiiController', function($scope) {
@@ -48,3 +50,28 @@
 	ocolApp.controller('contactController', function($scope) {
 		$scope.message = 'Contact us';
 	});
+
+// DEMO FUNCTION DE TESTARE LA CONSTRUCTIE DINAMICA DE VIEW
+function populate_anunturi() {
+    for(var i = 0; i < 5; i++) {
+    var adDPanel = document.createElement('div');
+    adDPanel.className = 'panel panel-success';
+
+    var add_panel_header = document.createElement('div');
+    add_panel_header.className = 'panel-heading';
+
+    var add_panel_body = document.createElement('div');
+    add_panel_body.className = 'panel-body';
+
+
+    adDPanel.appendChild(add_panel_header);
+    adDPanel.appendChild(add_panel_body);
+//    iDiv.id = 'block';
+
+    var anunturi_col = document.getElementById('anunturi-col');
+    if (anunturi_col != null) {
+        anunturi_col.appendChild(adDPanel);
+    }
+
+    }
+}
